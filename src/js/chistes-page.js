7dbf861ff1e78191fb7db403fda414c03a0a688d
Alex Paredes <alexparedesr95@gmail.com>
2022-02-11 22:23:32 -0600

    FEAT: Created file where all the jokes logic will be managed, the HTML, events and methods.

---
+++ src/js/chistes-page.js
@@ -0,0 +1,56 @@
+import { obtenerChiste } from './http-provider';
+
+const body = document.body;
+let btnOtro, olList;
+let numeroChiste = 1;
+
+const crearChistesHtml = () => {
+
+    const html = `
+        <h1 class="mt-5">Chistes</h1>
+        <hr>
+
+        <button class="btn btn-primary">Otro chiste</button>
+
+        <ol class="mt-2 list-group"></ol>
+    `;
+
+    const divChistes = document.createElement( 'div' );
+    divChistes.innerHTML = html;
+
+    body.append( divChistes );
+
+};
+
+const eventos = () => {
+
+    olList = document.querySelector( 'ol' );
+    btnOtro = document.querySelector( 'button' );
+
+    btnOtro.addEventListener( 'click', async () => {
+
+        btnOtro.disabled = true;
+        dibujarChiste( await obtenerChiste() );
+        btnOtro.disabled = false;
+
+    });
+
+};
+
+const dibujarChiste = ( chiste ) => {
+
+    const olItem = document.createElement( 'li' );
+    olItem.innerHTML = `<b>${ numeroChiste } - ${ chiste.id }</b>: ${ chiste.value }`;
+    olItem.classList.add( 'list-group-item' );
+    olList.append( olItem );
+    numeroChiste++;
+
+};
+
+
+export const init = () => {
+    crearChistesHtml();
+    eventos();
+};
+
+

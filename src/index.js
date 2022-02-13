e22c31afd7c8b68e370f856603a4ad06f63383ee
Alex Paredes <alexparedesr95@gmail.com>
2022-02-13 00:08:40 -0600

    FEAT: Implemented method to create a new user.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import { init } from './js/chistes-page'; */
 import * as CRUD from './js/crud-provider';
 
 CRUD.getUsuario(2).then( console.log );
+
+CRUD.crearUsuario({
+    'name': 'Alex',
+    'job': 'developer'
+}).then( console.log );

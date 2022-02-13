5d6a368e06252f7ecf11b1cc6663298d9cc2cc07
Alex Paredes <alexparedesr95@gmail.com>
2022-02-12 23:58:26 -0600

    FEAT: Imported CRUD on index.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { init } from './js/usuarios-page';
+/* import { init } from './js/usuarios-page';
+import { init } from './js/chistes-page'; */
+import * as CRUD from './js/crud-provider';
 
-
-init();
+CRUD.getUsuario(2).then( console.log );

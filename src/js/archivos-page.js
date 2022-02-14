ce83fad6ddec88f0348ed3094239eb4f84798e86
Alex Paredes <alexparedesr95@gmail.com>
2022-02-13 20:43:32 -0600

    FEAT: Imported function to upload image to cloudinary and added result to render the uploaded image in the HTML.

--- src/js/archivos-page.js
+++ src/js/archivos-page.js
@@ -1,3 +1,5 @@
+import { subirImagen } from './http-provider';
+
 const body = document.body;
 let inputFile, imgFoto;
 
@@ -28,7 +30,7 @@ const eventos = () => {
     inputFile.addEventListener( 'change', ( event ) => {
 
         const file = event.target.files[ 0 ];
-        console.log(file)
+        subirImagen( file ).then( url => imgFoto.src = url );
 
     });
 
